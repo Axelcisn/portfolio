4704d59caa3daea7120d5d58f78b89ad476bcd4a
Axelcisn <axelcisn@gmail.com>
2025-08-17 16:30:13 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,11 @@ import { defineConfig } from 'vitest/config';
 import { fileURLToPath, URL } from 'node:url';
 
 export default defineConfig({
-  test: { environment: 'node', include: ['tests/**/*.spec.js'] },
+  test: {
+    environment: 'node',
+    include: ['tests/**/*.spec.js']
+  },
   resolve: {
-    alias: {
-      lib: fileURLToPath(new URL('./lib', import.meta.url))
-    }
+    alias: { lib: fileURLToPath(new URL('./lib', import.meta.url)) }
   }
 });
