0ca0166c48fd80c22651a5fe28971dbd17bec849
Axelcisn <axelcisn@gmail.com>
2025-08-17 16:17:40 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,15 @@
-// vite.config.js
 import { defineConfig } from 'vitest/config';
-import path from 'path';
+import path from 'node:path';
 
 export default defineConfig({
-  resolve: { alias: { lib: path.resolve(__dirname, 'lib') } },
-  test: { environment: 'node' }
+  resolve: {
+    alias: {
+      lib: path.resolve(__dirname, 'lib')
+    }
+  },
+  test: {
+    environment: 'node',
+    include: ['tests/**/*.spec.js'],
+    globals: true
+  }
 });
