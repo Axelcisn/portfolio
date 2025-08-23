8bd478bfdb6e129f4177d19c7d3fde10efdfd131
Axelcisn <axelcisn@gmail.com>
2025-08-23 07:35:57 +0200

    Delete hooks directory

--- hooks/useDebounce.js
+++
@@ -1,7 +0,0 @@
-"use client";
-import { useEffect, useState } from "react";
-export default function useDebounce(value, delay = 300) {
-  const [d, setD] = useState(value);
-  useEffect(() => { const t = setTimeout(() => setD(value), delay); return () => clearTimeout(t); }, [value, delay]);
-  return d;
-}
